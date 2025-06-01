d02e1e263eb9f7b85a973d9eff76428f03a3de45
Kristian Serrano <kristianserrano@gmail.com>
2025-06-01 16:14:33 -0400

    Adds illumination suggestions to Trait rolls

--- modules/swade-targeted-damage.mjs
+++ modules/swade-targeted-damage.mjs
@@ -34,11 +34,6 @@ Hooks.on('setup', () => {
     ]);
 });
 
-Hooks.on('ready', () => {
-    // Setup socket
-    //game.socket.on(`module.${MODULE_ID}`, TargetedDamageApplicator.renderTargetedDamageApp);
-});
-
 Hooks.on('swadePreCalcWounds', (actor, damageContext, woundsInflicted, statusToApply) => {
     return false;
 });
@@ -88,48 +83,125 @@ Hooks.on('renderChatMessageHTML', (message, html, context) => {
 
 });
 
-Hooks.on('preUpdateScene', (scene, changed, options, userId) => {
-    if (game.userId !== userId) return;
-
-    if (!changed.tokenVision) {
-        changed['flags.swade-targeted-damage.illumination'] = null;
-        return;
-    }
-
-    const pitchBlack = changed.environment.globalLight.darkness.max;
-    const illuminationRollModifiers = CONFIG.SWADE.prototypeRollGroups.find((rollGroup) => rollGroup.name === game.i18n.localize('SWADE.Illumination._name'))?.modifiers;
-    const darknessLevel = changed.environment.darknessLevel;
-    let illuminationMod;
-
-    if (darknessLevel >= pitchBlack || !changed.environment.globalLight.enabled) {
-        illuminationMod = illuminationRollModifiers.find((m) => m.label = game.i18n.localize('SWADE.Illumination.Pitch'));
-    } else if (darknessLevel >= pitchBlack / 2) {
-        illuminationMod = illuminationRollModifiers.find((m) => m.label = game.i18n.localize('SWADE.Illumination.Dark'));
-    } else if (darknessLevel >= 0.05) {
-        illuminationMod = illuminationRollModifiers.find((m) => m.label = game.i18n.localize('SWADE.Illumination.Dim'));
-    } else {
-        illuminationMod = null;
-    }
-
-    changed['flags.swade-targeted-damage.illumination'] = illuminationMod;
-});
-
-const swadePreRollHookEvents = ['swadePreRollSkill', 'swadePreRollAttribute'];
-
-for (const hookEvent of swadePreRollHookEvents) {
+for (const hookEvent of ['swadePreRollSkill', 'swadePreRollAttribute']) {
     Hooks.on(hookEvent, (actor, skill, roll, modifiers, options) => {
+        // Ignore all of this if the feature is disabled in settings.
         if (!game.settings.get('swade-targeted-damage', 'auto-illumination-penalties')) return;
 
-        const illuminationModifier = game.scenes.current.getFlag('swade-targeted-damage', 'illumination');
+        // Get the system's Illumination roll modifiers.
+        const illuminationRollModifiers = foundry.utils.deepClone(CONFIG.SWADE.prototypeRollGroups.find((rollGroup) => rollGroup.name === game.i18n.localize('SWADE.Illumination._name'))?.modifiers);
         const token = game.scenes.current.tokens.find((t) => t.actorId === actor.id);
+        // Get the tokens that the user is targeting.
+        // Check each one using the same script for the actor's token but label them as (Target) instead.
+        const targetedTokenIDs = game.user.targets.ids;
+        // If there's a token associated with this actor, get its contextual darkness level; this includes scene region contexts. Otherwise, use the scene's current global darkness level.
+        const sceneIllumination = canvas.effects.getDarknessLevel(token?.getSnappedPosition());
+        // Get the scene's Pitch Dark Threshold (Global Illumination Threshold in FVTT terms).
+        const pitchDarkLevel = game.scenes.current.environment.globalLight.darkness.max;
+        const formInputStep = 0.05;
 
-        if (illuminationModifier && token) {
-            const hasBrightLight = token.light.bright > 0;
-            const hasDimLight = token.light.dim > 0;
+        // This function fixes floating point decimals and rounds to the nearest step in possible darkness values.
+        function roundToNearestStep(number) {
+            return Math.round(number * 20) / 20;
+        }
 
-            if (!hasBrightLight && !hasDimLight) {
-                modifiers.push(illuminationModifier);
+        // Add darkness level ranges to each Illumination modifiers.
+        const dimIlluminationModifier = illuminationRollModifiers?.find((m) => m.label === game.i18n.localize('SWADE.Illumination.Dim'));
+        dimIlluminationModifier.levels = {
+            max: roundToNearestStep((pitchDarkLevel / 2) - formInputStep),
+            min: 0.05
+        };
+        const darknessIlluminationModifier = illuminationRollModifiers?.find((m) => m.label === game.i18n.localize('SWADE.Illumination.Dark'));
+        darknessIlluminationModifier.levels = {
+            max: roundToNearestStep(pitchDarkLevel - formInputStep),
+            min: roundToNearestStep(pitchDarkLevel / 2),
+        };
+        const pitchDarkIlluminationModifier = illuminationRollModifiers?.find((m) => m.label === game.i18n.localize('SWADE.Illumination.Pitch'));
+        pitchDarkIlluminationModifier.levels = {
+            max: 1,
+            min: pitchDarkLevel
+        };
+        // Get an illumination modifier based on the scene's current darkness level.
+        const sceneIlluminationModifier = illuminationRollModifiers.find((m) => sceneIllumination >= m.levels.min && sceneIllumination <= m.levels.max);
+
+        // If there's a darkness level set, and there's the Actor's token to consider, begin evaluating its own light source as well as nearby light sources.
+        if (sceneIllumination > 0 && sceneIlluminationModifier && (token || targetedTokenIDs.length)) {
+            // This function determines what kind of illumination from nearby light sources the token is in.
+            function getProximityLight(token) {
+                let c = Object.values(token.object.center);
+                let lights = canvas.effects.lightSources.filter(src => !(src instanceof foundry.canvas.sources.GlobalLightSource) && src.shape.contains(...c));
+
+                // If there are no light sources, just return false
+                if (!lights.length) return false;
+
+                // Determine if the token is in any bright light.
+                let inBright = lights.some(light => {
+                    let { data: { x, y }, ratio } = light;
+                    let bright = foundry.canvas.geometry.ClockwiseSweepPolygon.create({ 'x': x, 'y': y }, {
+                        type: 'light',
+                        boundaryShapes: [new PIXI.Circle(x, y, ratio * light.shape.config.radius)]
+                    });
+                    return bright.contains(...c);
+                });
+
+                // If it's in bright light, return 'bright'.
+                if (inBright) return 'bright';
+
+                // Otherwise, the assumption at this point is that it's in dim light.
+                return 'dim';
             }
+
+            function checkTokenLighting(token, options = { isTargeted: false }) {
+                // Get the dim and bright light sources the token might have.
+                const hasBrightLight = token.light.bright > 0;
+                const hasDimLight = token.light.dim > 0;
+
+                // If the token doesn't have any light source, then let's define which Illumination penalty to apply, if any.
+                if (!hasBrightLight) {
+                    // Get any proximity light conditions that the token might be in. This will include token and ambient light sources.
+                    const proximityLight = getProximityLight(token);
+
+                    // If the token is in any light that's in proximity, make the scene darkness penalty optional.
+                    if (proximityLight) sceneIlluminationModifier.ignore = true;
+
+                    // Append the source to the label
+                    const label = `${dimIlluminationModifier.label} ${game.i18n.format("SWADETargetedDamage.IlluminationModifierLabels.Contexts.AroundToken", { name: token.name })}`;
+
+                    if ((hasDimLight || proximityLight === 'dim') && !modifiers.some((m) => m.label === label)) {
+                        // If the token is in Dim lighting, push in that option for a possible modifier.
+                        const dimLightModifier = foundry.utils.deepClone(dimIlluminationModifier);
+                        // Set the label.
+                        dimLightModifier.label = label;
+
+                        // If the user is targeting a token, it's likely not necessary to apply any Illumination penalties for where the Actor is since its the target's lighting that matters, so set its modifier to be inactive by default. Additionally, if the token is targeted and the user has multiple targets selected, do the same thing.
+                        if ((!options.isTargeted && game.user.targets.size) || (options.isTargeted && game.user.targets.size > 1)) {
+                            dimLightModifier.ignore = true;
+                        }
+
+                        modifiers.push(dimLightModifier);
+
+                        // Set the scene darkness penalty to optional since this token has light.
+                        sceneIlluminationModifier.ignore = true;
+                    }
+                } else {
+                    // If it has bright light, set the scene's illumination penalty to be optional.
+                    sceneIlluminationModifier.ignore = true;
+                }
+            }
+
+            // First check the Actor's token lighting situation.
+            checkTokenLighting(token);
+
+            // Now check the user's targets lighting situations.
+            for (const targetID of targetedTokenIDs) {
+                const target = game.scenes.current.tokens.get(targetID);
+                checkTokenLighting(target, { isTargeted: true });
+            }
+
+            // Append the source to the label
+            sceneIlluminationModifier.label = `${sceneIlluminationModifier.label} ${game.i18n.format("SWADETargetedDamage.IlluminationModifierLabels.Contexts.GlobalIllumination", { name: token.name })}`;
+            // Finally push in the scene's illumination modifier.
+            modifiers.push(sceneIlluminationModifier);
         }
     });
 }
